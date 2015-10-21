708d31ee3c6e8ef604d20dd51e109e7b939119ac
GDRMc <gdrdu86@gmail.com>
2015-10-21 10:06:30 +0200

    TP7 FIRST PART

--- src/s3/tp7calc/Test.java
+++ src/s3/tp7calc/Test.java
@@ -1,19 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package s3.tp7calc;
-
-/**
- *
- * @author USER
- */
-public class Test {
-    
-    public static void main(String[]args){
-        Expression exp1 = new OperationBinaire('*', new OperationBinaire('~', new OperationBinaire('+', new Operande(3), new Operande(2)), new OperationBinaire('+', new Operande(4), new Operande(-1))));
-        
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package s3.tp7calc;
+//
+///**
+// *
+// * @author USER
+// */
+//public class Test {
+//    
+//    public static void main(String[]args){
+//        Expression exp1 = new OperationBinaire('*', new OperationBinaire('~', new OperationBinaire('+', new Operande(3), new Operande(2)), new OperationBinaire('+', new Operande(4), new Operande(-1))));
+//        
+//    }
+//    
+//}
