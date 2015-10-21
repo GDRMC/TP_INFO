f52e13cf6f1c80299f6ed6342edee5cd1467b8e4
GDRMc <gdrdu86@gmail.com>
2015-10-21 08:04:34 +0200

    TP7

---
+++ src/s3/tp7calc/Expression.java
@@ -0,0 +1,90 @@
+package s3.tp7calc;
+
+abstract class Expression {
+    
+    abstract boolean estOperande();
+    abstract boolean estOperation();
+    abstract boolean estOperationUnaire();
+    abstract boolean estOperationBinaire();
+    abstract int getValeur();
+    abstract char getOperateur();
+    
+}
+
+class OperationBinaire extends Expression {
+    private char op;
+    private Expression expG;
+    private Expression expD;
+
+    boolean estOperande() {
+        return
+    }
+    boolean estOperation() {
+    
+    }
+    boolean estOperationUnaire() {
+        
+    }
+    boolean estOperationBinaire() {
+       
+    }
+    
+    int getValeur() {
+        
+    }
+    
+    char getOperateur() {
+        
+    }
+}
+
+class OperationUnaire extends Expression {
+    private char op;
+    private Expression exp;
+    
+    boolean estOperande() {
+        return false;
+    }
+    boolean estOperation() {
+        return true;
+    }
+    boolean estOperationUnaire() {
+        
+    }
+    boolean estOperationBinaire() {
+       
+    }
+
+    int getValeur() {
+        
+    }
+
+    char getOperateur() {
+        
+    }
+}
+
+class Operande extends Expression{
+    private int val;
+
+    boolean estOperande() {
+        return
+    }
+    boolean estOperation() {
+    
+    }
+    boolean estOperationUnaire() {
+        
+    }
+    boolean estOperationBinaire() {
+       
+    }
+
+    int getValeur() {
+        
+    }
+
+    char getOperateur() {
+        
+    }
+}
