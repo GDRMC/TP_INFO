86bcfbbeb1f5384c9cfde9b1848c402ee29e334d
GDRMc <gdrdu86@gmail.com>
2015-11-04 08:22:28 +0100

    TP8 - Ajout fonction test

---
+++ src/s3/tp8qsort/test.java
@@ -0,0 +1,9 @@
+package s3.tp8qsort;
+
+public class test {
+    public static void main(String[]args){
+        Liste l = new ListeCons(100);
+        l.QS();
+        System.out.println("lel");
+    }
+}
