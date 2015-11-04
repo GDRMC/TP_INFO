b0cf08a314506e4b48e5e8719eb425b0e70141a3
GDRMc <gdrdu86@gmail.com>
2015-11-04 09:34:51 +0100

    TP8 - Dicho, disfonctionnel

--- src/s3/tp8qsort/QStore.java
+++ src/s3/tp8qsort/QStore.java
@@ -2,33 +2,69 @@ package s3.tp8qsort;
 
 import java.util.ArrayList;
 
+//Ne marche pas à cause d'un caractère spécial dans le fichier mais totalement introuvable, et une stack overflow sur l'appel récursif de dicho().
+
 public class QStore {
 
     public static final int N = 2100;   // Nombre d'elements dans la liste au depart
 //---------------------------------------------------------------------------
-    private ArrayList<Integer> T;
+    private ArrayList<Integer> t;
 //---------------------------------------------------------------------------
     public int compteurDicho; // voir la dichotomie O(lg N )
 //---------------------------------------------------------------------------
 
     QStore() {
         this.compteurDicho = 0;
-        T = new ArrayList();
+        t = new ArrayList();
     }
 //---------------------------------------------------------------------------
 
     public void exec() {
-	// test
+        Liste l = new ListeCons(20);
+        Liste lsorted = l.QS();
+        ArrayList<Integer> arrayS = new ArrayList();
+        lsorted.listeEnTab(arrayS); //A BREAKPOINT POUR VOIR LE TABLEAU TRANQUILOU DANS LE DEBUGUEUR
+        int valeurrecherchee = 80000;
+        System.out.println("Valeur recherchée = " + valeurrecherchee);
+        this.Dicho(valeurrecherchee, 1, 100000);
     }
 
 // recherche Dichotomique dans le tableau -----------
     public boolean Dicho(int n, int iDebut, int iFin) {
+        int value = ((iFin - iDebut) / 2) + iDebut;
+        this.compteurDicho++;
+        boolean isInTab = false; //variable de présence de l'élément dans le tableau (initialement à false)
 
+        System.out.println("Appel " + compteurDicho + " - iDebut=" + iDebut + " - milieu=" + value + " - iFin=" + iFin);
+
+        if (value < n) {
+            //INFERIEUR
+            Dicho(n, iDebut, value);
+        } else if (value > n) {
+            //SUPERIEUR
+            Dicho(n, value, iFin);
+        } else if (value == n){
+            //VALEUR EGALE
+            isInTab = true; //passe la variable de présence à true
+        }
+        if (isInTab) {
+            System.out.println("Valeur trouvée sur value=" + value);
+        } else {
+            System.out.println("Valeur demandée introuvable...");
+        }
+        return isInTab;
     }
 
-// methode EstTri� ----------------------------------
     public boolean estTrie() {
-        return false;
+        int i = 0;
+        boolean retour = true;
+        while (i < t.size() && retour == false) {
+            if (t.get(i) < t.get(i + 1)) {
+                i++;
+            } else {
+                retour = false;
+            }
+        }
+        return retour;
     }
-
 }
--- src/s3/tp8qsort/test.java
+++ src/s3/tp8qsort/test.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class test {
     public static void main(String[]args){
+        //PARTIE 1 LISTE
         //LANCER AVEC UN BREAKPOINT SUR LE SOP DERNIERE LIGNE (OU PAS, au choix D:)
         Liste l = new ListeCons(10);
         Liste sorted= l.QS();
@@ -11,5 +12,11 @@ public class test {
         sorted.afficher();
         ArrayList<Integer> tab = new ArrayList();
         sorted.listeEnTab(tab); //A BREAKPOINT POUR VOIR LE TABLEAU TRANQUILOU DANS LE DEBUGUEUR
+        
+        //PARTIE 2 QSTORE
+        System.out.println("\nQSTORE TEST:");
+        QStore qs = new QStore();
+        qs.exec();
+        
     }
 }
